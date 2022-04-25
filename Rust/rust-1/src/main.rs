5b4b7bfd95f831e8dc4735c1d2e3707857fed6d6
PatrykDz <dziarneckisp04@wp.pl>
2022-04-25 21:19:50 +0000

    Gitpod setup

--- Rust/rust-1/src/main.rs
+++ Rust/rust-1/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
     println!("Hello, world!");
+
+    let sum = 2 + 2;
+    println!("The sum is: {}", sum);
 }
---
+++ Rust/rust-web-server-1/src/main.rs
@@ -0,0 +1,28 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
+fn main() {
+  let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+  println!("Listening for connections...");
+  
+  for stream in listener.incoming() {
+    let s: TcpStream = stream.unwrap();
+
+    println!("Connection extablished!");
+    println!("{}", s.peer_addr().unwrap());
+
+    handle_connection(s)
+  }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+  let mut buffer: [u8; 1024] = [0; 1024];
+  stream.read(&mut buffer).unwrap();
+  println!("Request: {}",
+          String::from_utf8_lossy(&buffer[..]));
+
+  let response = "HTTP/1.1 200 OK\r\n\r\n";
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
