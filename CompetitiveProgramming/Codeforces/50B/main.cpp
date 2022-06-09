01e79128aae4cf389e8e3f827f69696acf2a2e0a
PatrykDz <dziarneckisp04@wp.pl>
2022-06-09 22:17:16 +0000

    50B

---
+++ CompetitiveProgramming/Codeforces/50B/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string S;
+
+  cin >> S;
+
+  int n = S.size();
+  int numberOfPairs = 0;
+
+  // for (int i = 0; i < n; i++) {
+  //   for (int j = 0; j < n; j++) {
+  //     if (S[i] == S[j]) {
+  //       nubmerOfPairs++;
+  //     }
+  //   }
+  // }
+
+  map<string, int> dictionary;
+
+  for (int i = 0; i < n; i++) {
+    dictionary[to_string(S[i])] += 1;
+  }
+
+  map<string, int>::iterator it;
+  for (it = dictionary.begin(); it != dictionary.end(); it++) {
+    numberOfPairs += pow(it->second, 2);
+  }
+
+  cout << numberOfPairs;
+}
