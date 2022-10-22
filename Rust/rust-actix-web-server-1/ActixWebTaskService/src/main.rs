75f9818498f658adc20ba838c615d7669c30293f
PatrykDz <dziarneckisp04@wp.pl>
2022-10-22 10:12:15 +0000

    Rust wip

--- Rust/rust-actix-web-server-1/ActixWebTaskService/src/main.rs
+++ Rust/rust-actix-web-server-1/ActixWebTaskService/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// https://www.youtube.com/watch?v=L8tWKqSMKUI
+mod api;
+
+use api::task::{
+    get_task
+};
+
+use actix_web::{HttpServer, App, web::Data, middleware::Logger};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new()
+        .wrap(logger)
+        .service(get_task)
+    })
+    .bind(("127.0.0.1", 80))?
+    .run()
+    .await
+
 }
