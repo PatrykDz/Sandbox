7b9c941e69ffe45c0b9de42bb25c5c04f6d7c532
PatrykDz <dziarneckisp04@wp.pl>
2023-05-31 18:11:02 +0000

    Introduced .net Kafka producer

---
+++ Kafka/Producer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHostedService<KafkaProducerHostedService>();
+            });
+        }
+    }
+
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "patrykdz-fantastic-fortnight-vgw9rrxvxpcxgxr-9092.preview.app.github.dev:9092"
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var value = $"Hello World {i}";
+
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("demo", new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+
+            _producer.Flush(TimeSpan.FromSeconds(10));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
--- Kafka/setup.md
+++ Kafka/setup.md
@@ -3,3 +3,7 @@
 ## Setup Video
 https://www.youtube.com/watch?v=n_IQq3pze0s
 
+# Kafka UI - koncil
+```bash
+docker run -d -p 80:8080 -e bootstrapServers="kafka:9092" -e kouncil.auth.active-provider="inmemory" consdata/kouncil:latest
+```
